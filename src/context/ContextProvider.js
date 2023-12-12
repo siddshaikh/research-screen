24206b5e6e83d303e1c8e5b24e93eaf777ba45bf
siddeek shaikh <siddeekshaikh97@gmail.com>
2023-12-12 15:04:46 +0530

    bug fixed

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 
 export const ResearchContext = createContext(null);
 const ContextProvider = ({ children }) => {
